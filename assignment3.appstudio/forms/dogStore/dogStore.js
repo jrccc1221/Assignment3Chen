b21261ac75719eb72380c259372f154b6df34b25
Jiarong Chen <jrccc12345@gmail.com>
2021-03-14 22:16:22 -0500

    update

--- assignment3.appstudio/forms/dogStore/dogStore.js
+++ assignment3.appstudio/forms/dogStore/dogStore.js
@@ -1,20 +1,16 @@
-let productName = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
-let productPrice = [10, 14, 20, 30, 32, 27, 12, 27]
+let products = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
+let prices = [10, 14, 20, 30, 32, 27, 12, 27]
 let wishList = []
-let item = []
 
 
-btnShowInventory.onclick=function(){
-  for (i=0; i<=productName.length-1; i++){
-    item.push(`${productName[i]} : ${productPrice[i]}`)}
-    lblInventory.value = `${item}`
-}
+inptitem.oninput = Function()
+let listwish = inptitem.value
+wishList.push(products[wishList])
+wishList.push(prices[wishList])
 
-btnAddToWishList.onclick=function(){
-  wantedItem = inptItemSearch.value
-  wishList.push(wantedItem)
-  lblWishList.value = `${wishList}`
-}
+
+btnList.onclick=Function()
+alert(`${wishList}`)
 
 
 
