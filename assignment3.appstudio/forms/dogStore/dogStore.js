41bf2459b8feaab7828344c2970db06f1090571b
Jiarong Chen <jrccc12345@gmail.com>
2021-03-14 20:35:10 -0500

    update

--- assignment3.appstudio/forms/dogStore/dogStore.js
+++ assignment3.appstudio/forms/dogStore/dogStore.js
@@ -1,16 +1,20 @@
-let products = ['Crocheted Dog Toy', 'Slow Feeder Bowl', 'Pawz Grey Treat Jar', 'Blue Sweet Dreams Thermal Pajamas', 'Checker Chewy Vuiton Bowl', 'Red Heart Sweater Dress', 'Velvet Hair Bow - Sun Kissed', 'Into the Wild Leash']
-let prices = ['$10.00', '$14.00', '$20.00', '$30.00', '$32.00', '$27.00', '$12.00', '$27.00']
+let productName = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
+let productPrice = [10, 14, 20, 30, 32, 27, 12, 27]
 let wishList = []
-let things = []
-showBtn.onclick=function(){
-  for (let i = 0; i<= prices.length; i++) {
-    things.push(`${products[i]} : ${prices[i]}`)
-    }
-    lbl.value= `${item}`
-    }
-btn.onclick=function(){
-  let product = things.value
-  wishList.push(product)
-  wishes.value  = `${wishList}`
+let item = []
+
+
+btnShowInventory.onclick=function(){
+  for (i=0; i<=productName.length-1; i++){
+    item.push(`${productName[i]} : ${productPrice[i]}`)}
+    lblInventory.value = `${item}`
 }
+
+btnAddToWishList.onclick=function(){
+  wantedItem = inptItemSearch.value
+  wishList.push(wantedItem)
+  lblWishList.value = `${wishList}`
+}
+
+
 
